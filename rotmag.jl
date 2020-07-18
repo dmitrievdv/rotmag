a3423145bc1008d4bfc060d5a10df07aa129a0a1
dmitrievdv <dmitrievdv242@gmail.com>
2020-07-19 01:22:23 +0300

    Velocity Jacobian almost finished

--- rotmag.jl
+++ rotmag.jl
@@ -39,21 +39,32 @@ struct Magnetosphere{T<:Real}
 	end
 end
 
-
 function dipolermax(r, θ)
 	return r/sin(θ)^2
 end
 
-function freefallvelocity(r, r_max, star)
-	return star.v_esc*√(1/r - 1/r_max)
+function freefallvelocity(r, θ, star)
+	return star.v_esc*√(1/r(1 - sin(θ)^2))
+end
+
+function freefallvelocitygradient(r, θ, star)
+	dvdθ = -star.v_esc*√(1/r)/√(1-sin(θ)^2)*sin(θ)*cos(θ)
+	dvdr = -0.5*star.v_esc*√(1 - sin(θ)^2)*√(1/r^3)
+	return [dvdr, dvdθ]
 end
 
 function surfmagfield(M_dot, R_in, star)
 	return 4.2e2*(R_in/2.2)*(star.M/(0.5*M☉))^0.25*(M_dot/1e-8)^0.5*(star.R/(2*R☉))^(-3)
 end
 
-function dipolefield(surffield, r, r_max)
-	return surffield*(4-3*r/r_max)^0.5/r^3
+function dipolefield(surffield, r, θ)
+	return surffield*(4-3*sin(θ)^2)^0.5/r^3
+end
+
+function  dipolefieldgradient(surffield, r, θ)
+	dBdr = -3*surffield*(4-3*sin(θ)^2)^0.5/r^4
+	dBdθ = -3*surffield/r^3/(4-3*sin(θ)^2)^0.5*sin(θ)*cos(θ)
+	return [dBdr, dBdθ]
 end
 
 function eta(M_dot, r_mi, r_mo, star)
@@ -61,57 +72,82 @@ function eta(M_dot, r_mi, r_mo, star)
 	return 2.6e-9*(B_star/1e3)^(-1)*(M_dot/1e-8)*(star.R/(2*R☉))^-2*(1/r_mi-1/r_mo)^(-1)
 end
 
-function solvepoint(r_m :: Real, θ :: Real, star :: Star, mag :: Magnetosphere)
+function nonsolid_rotation(r_m :: Real, θ :: Real, star :: Star, mag :: Magnetosphere)
 	r = r_m*sin(θ)^2
 	R = r*sin(θ)
 	η = eta(mag.M_dot, mag.r_mi, mag.r_mo, star)
 	B_star = surfmagfield(mag.M_dot, mag.r_mi, star)
-	# solve_start = [star.v_esc/B_star, 0]
-	Bp = dipolefield(B_star, r, r_m)
-	v_ff = freefallvelocity(r, r_m, star)
+	Bp = dipolefield(B_star, r, θ)
+	v_ff = freefallvelocity(r, θ, star)
 	v_eq = star.v_eq
 	v_po = mag.v_start
-	r_m2R2 = r_m^2 - R^2
-	mech_E = v_po^2 + v_ff^2 - v_eq^2*r_m2R2
-	# print(B_star, " ", Bp, " ", η, " ", 1e-5*v_ff, "\n")
-	p = Polynomial([(r_m2R2)^2*mech_E, -2R*(r_m2R2)*mech_E, (R^2*mech_E - r_m2R2^2*v_eq^2 - (Bp*R/(4*π*η))^2), 
-		            2*R*(r_m2R2)*v_eq^2, -R^2*v_eq^2])
-	p = p/(R^2*v_eq^2)
-	# println(Bp/η)
-	x = roots(p)
-	# println(x)
+	ρ = r_m^2 - R^2
+	E = v_po^2 + v_ff^2 - v_eq^2*ρ
+	P = Polynomial([ρ^2*E, -2R*ρ*E, (R^2*E - ρ^2*v_eq^2 - (Bp*R/(4*π*η))^2), 
+		            2*R*ρ*v_eq^2, -R^2*v_eq^2])
+	# p = p/(R^2*v_eq^2)
+	x = roots(P)
 	xreal = real.(x[imag.(x) .≈ 0])
-	# println(p.(xreal))
-	# println(xreal)
 	xreal = xreal*v_eq
 	vt = sum(abs.(xreal))/2
-	k = @. √(mech_E - xreal^2)/Bp
+	k = @. √(E - xreal^2)/Bp
 	Bϕ = @. xreal/k
-	# println("k: ", k)
-	# println("Bϕ: ", Bϕ)
-	# println(mech_E, " ", xreal.^2, " ", Bp^2)
-	ang_conservation = @. abs(v_eq*r_m2R2/(Bϕ*R*(k - (4*π*η)^(-1)))-1)
-	# eng_conservation = @. (k^2*Bp*2 + xreal^2)-(mech_E)
-	# println("ang: ", ang_conservation)
-	# println("eng: ", eng_conservation)
+	ang_conservation = @. abs(v_eq*ρ/(Bϕ*R*(k - (4*π*η)^(-1)))-1)
 	vt = xreal[ang_conservation .< 1e-10][1]
-    # println(R, " ", r_m, " ", vt/1e5, " ", k*Bp/1e5)
-	return vt
+    return vt
 end
+
+function velocity_jacobian(r_m :: Real, θ :: Real, v_t :: Real, v_p :: Real, star :: Star, mag :: Magnetosphere)
+	r = r_m*sin(θ)^2
+	R = r*sin(θ)
+	η = eta(mag.M_dot, mag.r_mi, mag.r_mo, star)
+	B_star = surfmagfield(mag.M_dot, mag.r_mi, star)
+	Bp = dipolefield(B_star, r, θ)
+	∇Bp = dipolefieldgradient(B_star, r, θ)
+	v_ff = freefallvelocity(r, θ, star)
+	∇v_ff = freefallvelocitygradient(r, θ, star)
+	v_eq = star.v_eq
+	v_po = mag.v_start
+	∇r_m = [1/sin(θ)^2, -2r/sin(θ)^3*cos(θ)]
+	∇R = [sin(θ), r*cos(θ)]
+	ρ = r_m^2 - R^2
+	∇ρ = @. 2r_m*∇r_m - 2R*∇R
+	E = v_po^2 + v_ff^2 - v_eq^2*ρ
+	∇E = @. 2*v_ff*∇v_ff - v_eq^2*∇ρ
+	P = Polynomial([ρ^2*E, -2R*ρ*E, (R^2*E - ρ^2*v_eq^2 - (Bp*R/(4*π*η))^2), 
+					2*R*ρ*v_eq^2, -R^2*v_eq^2])
+	dP = derivative(P)
+	∇P = @. Polynomial(vcat(2*ρ*∇ρ + ρ^2*∇E, -2(∇R*E*ρ + R*∇E*ρ + R*E*∇ρ), 
+						 (2R*∇R*E + R^2*∇E - 2v_eq^2*ρ*∇ρ - 2Bp*R/(4*π*η)^2*(∇Bp*R + Bp*∇R)),
+						   2R*∇ρ*v_eq^2 + 2ρ*∇R*v_eq^2, -2R*∇R*v_eq^2))
+	x = v_t/v_eq
+	∇v_t = -v_eq*[∇P[1](x)/dP(x), ∇P[2](x)/dP(x)]
+	∇v_p = v_t/v_p*∇v_t
+	return ∇v_t
+end
+
 
 function solvemag(star :: Star, mag :: Magnetosphere; n_r = 5 :: Int, n_θ = 20 :: Int, file = "rotmag.dat")
 	out = open(file, "w")
-	B_star = surfmagfield(mag.M_dot, mag.r_mi, star)		
+	B_star = surfmagfield(mag.M_dot, mag.r_mi, star)
+	θ_grid = zeros((n_θ, n_r))
+	r_m_grid = zeros((n_θ, n_r))
+	v_p_grid = zeros((n_θ, n_r))
+	v_t_grid = zeros((n_θ, n_r))
 	for i=1:n_r
 		r_m = mag.r_mi + (i-1)*(mag.r_mo-mag.r_mi)/(n_r-1)
 		θ_s = asin(√(1/r_m))
 		# solve_start = [star.v_esc/B_star, 0]
 		for j=1:n_θ
 			θ = π/2 - (j)*(π/2 - θ_s)/(n_θ)
+			r_m_grid[j,i] = r_m
+			θ_grid[j,i] = θ
 			B_p = dipolefield(B_star, r_m*sin(θ)^2, r_m)
-			v_t = solvepoint(r_m, θ, star, mag)
+			v_t = nonsolid_rotation(r_m, θ, star, mag)
+			v_t_grid[j,i] = v_t
 			v_ff = freefallvelocity(r_m*sin(θ)^2, r_m, star)
 			v_p = √(mag.v_start^2 + v_ff^2 - v_t^2)
+			v_p_grid[j,i] = v_p
 			v_rot = star.v_eq*r_m*sin(θ)^3
 			solve_start = [v_p/B_p, v_t/v_p*B_p]
 			@printf(out, "%8.f %8.f %8.f %8.f %8.f %8.f\n", r_m, θ, v_p*1e-5, v_t*1e-5, v_ff*1e-5, v_rot*1e-5)
@@ -119,7 +155,7 @@ function solvemag(star :: Star, mag :: Magnetosphere; n_r = 5 :: Int, n_θ = 20 
 		print(out, "\n")
 	end
 	close(out)
-	return 0
+	return r_m_grid, θ_grid, v_p_grid, v_t_grid
 end
 end
 
--- test.jl
+++ test.jl
@@ -1,6 +1,15 @@
+using Plots
+
 include("rotmag.jl")
 import .Rotmag
 
 star = Rotmag.Star(2.0, 0.5, 15)
 mag = Rotmag.Magnetosphere(2.2, 3, 1e-7, 10)
-Rotmag.solvemag(star, mag)
+r_ms, θs, v_ps, v_ts = Rotmag.solvemag(star, mag)
+Rs = @. r_ms*sin(θs)^3
+
+
+plot(Rs, v_ps/1e5, linecolor = :red)
+plot!(Rs, v_ts/1e4, linecolor = :blue)
+
+
