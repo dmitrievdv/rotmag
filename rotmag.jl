30e90cf01f6beae1b3092a94c7b55ec04f192127
dmitrievdv <dmitrievdv242@gmail.com>
2020-07-21 00:24:24 +0300

    Almost done, very cool

--- rotmag.jl
+++ rotmag.jl
@@ -1,9 +1,9 @@
 module Rotmag
 using Printf
 using Polynomials
+using LinearAlgebra
 
 # export solvemag, solvepoint, test_export, R☉
-
 
 const R☉ = 6.955e10
 const M☉ = 1.989e33
@@ -35,7 +35,7 @@ struct Magnetosphere{T<:Real}
 			_r_mo = float(r_mo)
 			_M_dot = float(M_dot)
 			_v_start = float(v_start)
-    	new{typeof(r_mi)}(_r_mi, _r_mo, _M_dot, _v_start*1e5)
+    	new{typeof(_r_mi)}(_r_mi, _r_mo, _M_dot, _v_start*1e5)
 	end
 end
 
@@ -44,7 +44,7 @@ function dipolermax(r, θ)
 end
 
 function freefallvelocity(r, θ, star)
-	return star.v_esc*√(1/r(1 - sin(θ)^2))
+	return star.v_esc*√(1/r*(1 - sin(θ)^2))
 end
 
 function freefallvelocitygradient(r, θ, star)
@@ -72,8 +72,21 @@ function eta(M_dot, r_mi, r_mo, star)
 	return 2.6e-9*(B_star/1e3)^(-1)*(M_dot/1e-8)*(star.R/(2*R☉))^-2*(1/r_mi-1/r_mo)^(-1)
 end
 
-function nonsolid_rotation(r_m :: Real, θ :: Real, star :: Star, mag :: Magnetosphere)
-	r = r_m*sin(θ)^2
+function constructpolynomial(ρ, E, R, v_eq, Bp, η)
+	P = Polynomial([ρ^2*E, -2R*ρ*E, (R^2*E - ρ^2*v_eq^2 - (Bp*R/(4*π*η))^2), 
+					2*R*ρ*v_eq^2, -R^2*v_eq^2])
+    return P
+end
+
+function polynomialgradient(ρ, ∇ρ, E, ∇E, R, ∇R, B_p, ∇B_p, v_eq, η)
+	∇P = @. Polynomial(vcat(2*ρ*∇ρ*E + ρ^2*∇E, -2(∇R*E*ρ + R*∇E*ρ + R*E*∇ρ), 
+						 (2R*∇R*E + R^2*∇E - 2v_eq^2*ρ*∇ρ - 2B_p*R/(4*π*η)^2*(∇B_p*R + B_p*∇R)),
+						   2R*∇ρ*v_eq^2 + 2ρ*∇R*v_eq^2, -2R*∇R*v_eq^2))
+	return ∇P 
+end
+
+function nonsolidrotation(r :: Real, θ :: Real, star :: Star, mag :: Magnetosphere)
+	r_m = r/sin(θ)^2
 	R = r*sin(θ)
 	η = eta(mag.M_dot, mag.r_mi, mag.r_mo, star)
 	B_star = surfmagfield(mag.M_dot, mag.r_mi, star)
@@ -83,27 +96,27 @@ function nonsolid_rotation(r_m :: Real, θ :: Real, star :: Star, mag :: Magneto
 	v_po = mag.v_start
 	ρ = r_m^2 - R^2
 	E = v_po^2 + v_ff^2 - v_eq^2*ρ
-	P = Polynomial([ρ^2*E, -2R*ρ*E, (R^2*E - ρ^2*v_eq^2 - (Bp*R/(4*π*η))^2), 
-		            2*R*ρ*v_eq^2, -R^2*v_eq^2])
+	P = constructpolynomial(ρ, E, R, v_eq, Bp, η)
 	# p = p/(R^2*v_eq^2)
 	x = roots(P)
 	xreal = real.(x[imag.(x) .≈ 0])
 	xreal = xreal*v_eq
-	vt = sum(abs.(xreal))/2
+	# v_t = sum(abs.(xreal))/2
 	k = @. √(E - xreal^2)/Bp
 	Bϕ = @. xreal/k
 	ang_conservation = @. abs(v_eq*ρ/(Bϕ*R*(k - (4*π*η)^(-1)))-1)
-	vt = xreal[ang_conservation .< 1e-10][1]
-    return vt
+	v_t = xreal[ang_conservation .< 1e-10][1]
+	v_p = √(E - v_t^2)
+	return [v_p, v_t]
 end
 
-function velocity_jacobian(r_m :: Real, θ :: Real, v_t :: Real, v_p :: Real, star :: Star, mag :: Magnetosphere)
-	r = r_m*sin(θ)^2
+function velocityjacobian(r :: Real, θ :: Real, v_p :: Real, v_t :: Real, star :: Star, mag :: Magnetosphere)
+	r_m = r/sin(θ)^2
 	R = r*sin(θ)
 	η = eta(mag.M_dot, mag.r_mi, mag.r_mo, star)
 	B_star = surfmagfield(mag.M_dot, mag.r_mi, star)
-	Bp = dipolefield(B_star, r, θ)
-	∇Bp = dipolefieldgradient(B_star, r, θ)
+	B_p = dipolefield(B_star, r, θ)
+	∇B_p = dipolefieldgradient(B_star, r, θ)
 	v_ff = freefallvelocity(r, θ, star)
 	∇v_ff = freefallvelocitygradient(r, θ, star)
 	v_eq = star.v_eq
@@ -114,48 +127,113 @@ function velocity_jacobian(r_m :: Real, θ :: Real, v_t :: Real, v_p :: Real, st
 	∇ρ = @. 2r_m*∇r_m - 2R*∇R
 	E = v_po^2 + v_ff^2 - v_eq^2*ρ
 	∇E = @. 2*v_ff*∇v_ff - v_eq^2*∇ρ
-	P = Polynomial([ρ^2*E, -2R*ρ*E, (R^2*E - ρ^2*v_eq^2 - (Bp*R/(4*π*η))^2), 
-					2*R*ρ*v_eq^2, -R^2*v_eq^2])
+	P = constructpolynomial(ρ, E, R, v_eq, B_p, η)
 	dP = derivative(P)
-	∇P = @. Polynomial(vcat(2*ρ*∇ρ + ρ^2*∇E, -2(∇R*E*ρ + R*∇E*ρ + R*E*∇ρ), 
-						 (2R*∇R*E + R^2*∇E - 2v_eq^2*ρ*∇ρ - 2Bp*R/(4*π*η)^2*(∇Bp*R + Bp*∇R)),
-						   2R*∇ρ*v_eq^2 + 2ρ*∇R*v_eq^2, -2R*∇R*v_eq^2))
+	∇P = polynomialgradient(ρ, ∇ρ, E, ∇E, R, ∇R, B_p, ∇B_p, v_eq, η)
 	x = v_t/v_eq
 	∇v_t = -v_eq*[∇P[1](x)/dP(x), ∇P[2](x)/dP(x)]
-	∇v_p = v_t/v_p*∇v_t
-	return ∇v_t
+	∇v_p = @. 1/v_p*(∇E/2 - v_t*∇v_t)
+	# v_p = √(E - v_t^2)
+	return [∇v_p, ∇v_t]
 end
 
+function directionalgradient(r, θ, α, β, v_p, v_t, ∇v_p, ∇v_t)
+	n = [cos(α), 1/r*sin(α)*cos(β),  1/r/sin(θ)*sin(α)*sin(β)]
+	nT = [cos(α) 1/r*sin(α)*cos(β) 1/r/sin(θ)*sin(α)*sin(β)]
+	∇n = [      0             -n[2]/r              -n[3]/r;
+		      r*n[2]          -n[1]/r           -n[3]/tan(θ);
+		  r*sin(θ)^2*n[3] sin(θ)*cos(θ)*n[3] -n[1]/r - n[2]/tan(θ)]
+
+	ξ = √(4 - 3sin(θ)^2)
+	k = [-2cos(θ)/ξ, -r*sin(θ)/ξ, 0]
+	t = [0, 0, r*sin(θ)]
+	v = v_p*k + v_t*t
+
+	∇k = [     0         -sin(θ)/ξ   0; 
+		  2sin(θ)/ξ^3 -r*4cos(θ)/ξ^3 0;
+		       0          0          0]
+	
+	∇t = [ 0 0  sin(θ);
+		   0 0 r*cos(θ);
+		   0 0    0]
+
+	k∇v_p = [k[1]*∇v_p[1] k[2]*∇v_p[1] k[3]*∇v_p[1];
+			 k[1]*∇v_p[2] k[2]*∇v_p[2] k[3]*∇v_p[2];
+				 0            0            0]
+				 
+	t∇v_t = [0 0 t[3]*∇v_t[1];
+			 0 0 t[3]*∇v_t[2];
+			 0 0      0]
+
+	∇v = v_p*∇k + k∇v_p + v_t*∇t + t∇v_t
+	n∇vn = dot(n,(∇v*n + ∇n*v))
+	return n∇vn
+end
+
+function integrategradalldirections(r, θ, v_p, v_t, ∇v_p, ∇v_t; n=100)
+	αs = [0:π/n:(n-1)*π/n;] .+ π/(2*n)
+	βs = [0:2*π/n:(n-1)*2*π/n;] .+ π/n
+	I = 0
+	for α ∈ αs, β ∈ βs
+		I = I + abs(directionalgradient(r, θ, α, β, v_p, v_t, ∇v_p, ∇v_t))*sin(α)*π/n*2*π/n
+	end
+	return I
+end
+
+function integrategradstar(r, θ, v_p, v_t, ∇v_p, ∇v_t; n=100)
+	ϕ = 0
+	try
+		ϕ = asin(1/r)
+	catch e
+		if isa(e, DomainError)
+			ϕ = π/2
+		end
+	end
+
+	αs = -([0:ϕ/n:(n-1)*ϕ/n;] .- π) .+ ϕ/(2*n)
+	βs = [0:2*π/n:(n-1)*2*π/n;] .+ π/n
+	I = 0
+	for α ∈ αs, β ∈ βs
+		I = I + abs(directionalgradient(r, θ, α, β, v_p, v_t, ∇v_p, ∇v_t))*sin(α)*ϕ/n*2*π/n
+	end
+	return I
+end
+
+function calcsource(r, θ, v_p, v_t, ∇v_p, ∇v_t)
+	return (integrategradstar(r, θ, v_p, v_t, ∇v_p, ∇v_t)/
+	        integrategradalldirections(r, θ, v_p, v_t, ∇v_p, ∇v_t))
+end
 
 function solvemag(star :: Star, mag :: Magnetosphere; n_r = 5 :: Int, n_θ = 20 :: Int, file = "rotmag.dat")
-	out = open(file, "w")
+	# out = open(file, "w")
 	B_star = surfmagfield(mag.M_dot, mag.r_mi, star)
 	θ_grid = zeros((n_θ, n_r))
 	r_m_grid = zeros((n_θ, n_r))
 	v_p_grid = zeros((n_θ, n_r))
 	v_t_grid = zeros((n_θ, n_r))
+	source_grid = zeros((n_θ, n_r))
 	for i=1:n_r
 		r_m = mag.r_mi + (i-1)*(mag.r_mo-mag.r_mi)/(n_r-1)
 		θ_s = asin(√(1/r_m))
 		# solve_start = [star.v_esc/B_star, 0]
 		for j=1:n_θ
 			θ = π/2 - (j)*(π/2 - θ_s)/(n_θ)
+			r = r_m*sin(θ)^2
 			r_m_grid[j,i] = r_m
 			θ_grid[j,i] = θ
 			B_p = dipolefield(B_star, r_m*sin(θ)^2, r_m)
-			v_t = nonsolid_rotation(r_m, θ, star, mag)
+			v_p, v_t = nonsolidrotation(r, θ, star, mag)
 			v_t_grid[j,i] = v_t
-			v_ff = freefallvelocity(r_m*sin(θ)^2, r_m, star)
-			v_p = √(mag.v_start^2 + v_ff^2 - v_t^2)
 			v_p_grid[j,i] = v_p
 			v_rot = star.v_eq*r_m*sin(θ)^3
-			solve_start = [v_p/B_p, v_t/v_p*B_p]
-			@printf(out, "%8.f %8.f %8.f %8.f %8.f %8.f\n", r_m, θ, v_p*1e-5, v_t*1e-5, v_ff*1e-5, v_rot*1e-5)
+			∇v_p, ∇v_t = velocityjacobian(r, θ, v_p, v_t, star, mag)
+			source_grid[j,i] = calcsource(r, θ, v_p, v_t, ∇v_p, ∇v_t)
+			# @printf(out, "%8.f %8.f %8.f %8.f %8.f %8.f\n", r_m, θ, v_p*1e-5, v_t*1e-5, v_ff*1e-5, v_rot*1e-5)
 		end
-		print(out, "\n")
+		# print(out, "\n")
 	end
-	close(out)
-	return r_m_grid, θ_grid, v_p_grid, v_t_grid
+	# close(out)
+	return r_m_grid, θ_grid, v_p_grid, v_t_grid, source_grid
 end
 end
 
--- test.jl
+++ test.jl
@@ -3,13 +3,33 @@ using Plots
 include("rotmag.jl")
 import .Rotmag
 
-star = Rotmag.Star(2.0, 0.5, 15)
-mag = Rotmag.Magnetosphere(2.2, 3, 1e-7, 10)
-r_ms, θs, v_ps, v_ts = Rotmag.solvemag(star, mag)
+star_fast = Rotmag.Star(2.0, 0.5, 35)
+star_slow = Rotmag.Star(2.0, 0.5, 1)
+mag = Rotmag.Magnetosphere(2, 3, 1e-7, 10)
+
+function testderivative(r_m, θ, star, mag; eps = 1e-3)
+    r = r_m*sin(θ)^2
+    v_po, v_to = Rotmag.nonsolidrotation(r, θ, star, mag)
+    ∇v_p, ∇v_t = Rotmag.velocityjacobian(r, θ, v_po, v_to, star, mag)
+    dr = eps; dθ = eps
+    v_pr, v_tr = Rotmag.nonsolidrotation(r + dr, θ, star, mag)
+    v_pθ, v_tθ = Rotmag.nonsolidrotation(r, θ + dθ, star, mag)
+    ∇v_pcalc = [(v_pr - v_po)/dr, (v_pθ - v_po)/dθ]
+    ∇v_tcalc = [(v_tr - v_to)/dr, (v_tθ - v_to)/dθ]
+    println("∇v_p: ", ∇v_p)
+    println("∇v_p: ", ∇v_pcalc)
+    println("∇v_t: ", ∇v_t)
+    println("∇v_t: ", ∇v_tcalc)
+end
+
+# testderivative(2.5, π/4, star, mag, eps = 1e-5)
+
+r_ms, θs, v_ps, v_ts, fast_source_s = Rotmag.solvemag(star_fast, mag, n_r = 3)
 Rs = @. r_ms*sin(θs)^3
-
-
-plot(Rs, v_ps/1e5, linecolor = :red)
-plot!(Rs, v_ts/1e4, linecolor = :blue)
-
+rs = @. r_ms*sin(θs)^2
+plot(rs, fast_source_s, linecolor = :red)
+r_ms, θs, v_ps, v_ts, slow_source_s = Rotmag.solvemag(star_slow, mag, n_r = 3)
+plot!(rs, slow_source_s, linecolor = :blue)
+# plot(Rs, v_ps/1e5, linecolor = :red)
+# plot!(Rs, v_ts/1e4, linecolor = :blue)
 
